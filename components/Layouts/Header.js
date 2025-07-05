247208a8dda3c86ef6ae19a75b484c6718712ef9
Joseph Lyman <jmlyman424@gmail.com>
2025-07-05 16:47:07 -0400

    Header updates

--- components/Layouts/Header.js
+++ components/Layouts/Header.js
@@ -1,7 +1,14 @@
 import Link from 'next/link';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { MenuIcon } from '@heroicons/react/outline';
-import { Popover, Transition } from '@headlessui/react';
+import {
+  Popover,
+  PopoverButton,
+  PopoverPanel,
+  Transition,
+} from '@headlessui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 const navLinks = [
   {
@@ -10,13 +17,13 @@ const navLinks = [
     enabled: true,
   },
   {
-    path: '/#projects',
-    label: 'Projects',
+    path: '/#about',
+    label: 'About',
     enabled: true,
   },
   {
-    path: '/#resume',
-    label: 'Resume',
+    path: '/#portfolio',
+    label: 'Portfolio',
     enabled: true,
   },
   {
@@ -24,31 +31,48 @@ const navLinks = [
     label: 'Contact',
     enabled: true,
   },
-  {
-    path: '/test',
-    label: 'Test',
-    enabled: false,
-  },
 ];
 
-function NavLinks({ styles }) {
+function NavLinks({ className }) {
   return navLinks.map((navLinks) => {
     if (!navLinks.enabled) return null;
     return (
-      <Popover.Button as="li" key={navLinks.label}>
-        <Link href={navLinks.path} className={styles}>
+      <PopoverButton as="li" key={navLinks.label}>
+        <Link href={navLinks.path} className={className}>
           {navLinks.label}
         </Link>
-      </Popover.Button>
+      </PopoverButton>
     );
   });
+}
+
+function ToggleTheme() {
+  const checkIsDarkSchemePreferred = () =>
+    // window?.matchMedia?.('(prefers-color-scheme:dark)')?.matches ?? false;
+    false;
+
+  const [isDark, setIsDark] = useState(checkIsDarkSchemePreferred());
+  console.log('initial colorscheme: ', checkIsDarkSchemePreferred());
+
+  function handleThemeChange() {
+    setIsDark(!isDark);
+  }
+
+  return (
+    <FontAwesomeIcon
+      icon={isDark ? faSun : faMoon}
+      onClick={handleThemeChange}
+      className="fa-fw text-xl text-black dark:text-white hover:text-accent-1 cursor-pointer"
+    />
+  );
 }
 
 function DesktopMenu() {
   return (
     <Popover as="nav" className="hidden gap-4 my-auto md:flex">
-      <ul className="flex gap-4">
-        <NavLinks styles="py-2 px-6 text-gray-700 hover:text-black transition-all duration-200 cursor-pointer" />
+      <ul className="flex gap-6">
+        <NavLinks className="py-2 text-gray-700 dark:text-white font-semibold hover:text-accent-1 transition-all duration-200 cursor-pointer" />
+        {/* <ToggleTheme /> */}
       </ul>
     </Popover>
   );
@@ -59,9 +83,10 @@ function MobileMenu() {
     <Popover as={Fragment}>
       {({ open }) => (
         <>
-          <Popover.Button aria-label="menu">
-            <MenuIcon className="hover:translate-y-[.1rem] p-1 w-8 h-8 text-gray-500 hover:bg-gray-100 rounded cursor-pointer transition-all duration-500 md:hidden" />
-          </Popover.Button>
+          <ToggleTheme />
+          <PopoverButton aria-label="menu">
+            <MenuIcon className="p-1 w-8 h-8 text-gray-500 hover:bg-gray-100 rounded cursor-pointer transition-all duration-500 md:hidden" />
+          </PopoverButton>
 
           {open && (
             <Transition
@@ -73,16 +98,16 @@ function MobileMenu() {
               leaveFrom="transform scale-100 opacity-100"
               leaveTo="transform scale-95 opacity-0"
             >
-              <Popover.Panel
-                className="absolute z-10 right-0 top-1 flex justify-center mt-14 w-full h-screen bg-gray-900 overflow-hidden md:hidden"
+              <PopoverPanel
+                className="absolute z-10 right-0 top-1 flex justify-center mt-14 w-full h-screen bg-white dark:bg-darkmode overflow-hidden md:hidden"
                 static
               >
                 <nav className="flex my-auto">
                   <ul className="flex flex-col gap-12 text-center text-2xl">
-                    <NavLinks styles="py-2 px-6 text-white hover:text-gray-400 transition-all duration-200 cursor-pointer" />
+                    <NavLinks styles="py-2 px-6 text-black hover:text-accent-1 dark:text-white transition-all duration-200 cursor-pointer" />
                   </ul>
                 </nav>
-              </Popover.Panel>
+              </PopoverPanel>
             </Transition>
           )}
         </>
@@ -93,13 +118,15 @@ function MobileMenu() {
 
 export default function Header() {
   return (
-    <header className="sticky z-50 top-0 w-full bg-white shadow">
-      <div className="flex flex-wrap items-center justify-between align-middle mx-auto px-8 py-3 max-w-screen-2xl">
+    <header className="sticky z-50 top-0 w-full bg-white dark:bg-darkmode shadow">
+      <div className="flex flex-wrap items-center justify-between align-middle md:min-h-16 mx-auto px-8 py-3 max-w-screen-2xl">
         <div className="flex align-middle">
-          <Link href="/">Joseph Lyman</Link>
+          <Link href="/" className="font-semibold dark:text-white">
+            Joseph Lyman
+          </Link>
         </div>
 
-        <div className="flex align-middle">
+        <div className="flex align-middle gap-6">
           <DesktopMenu />
           <MobileMenu />
         </div>
